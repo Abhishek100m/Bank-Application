52774602f783421b3744e44b6ad5e04b29e57e3e
Abhishek Gupta <abhigupta7049612180@gmail.com>
2024-01-08 00:50:53 +0530

    x

--- src/com/bank/Demo.java
+++ src/com/bank/Demo.java
@@ -1,10 +1,7 @@
 package com.bank;
 
-import java.util.Date;
-
 public class Demo {
 	public static void main(String[] args) {
-	 Account account = new Account("x","1232323","abhi","xyz",0,null,new Date());
-	 System.out.println(account);
+
 	}
 }
